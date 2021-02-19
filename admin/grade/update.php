47d648129e5034345da5291a8000a3c2f2bcf1dd
Rifky Galuh <rfqgal@users.noreply.github.com>
2021-02-19 19:25:09 +0700

    created update function

---
+++ admin/grade/update.php
@@ -0,0 +1,44 @@
+<?php
+// Required Headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// Include Files
+include_once '../config/database.php';
+include_once '../objects/grade.php';
+
+// Get DB Connection
+$database = new Database();
+$db = $database->getConnection();
+
+// Prepare Object
+$grade = new Grade($db);
+
+// Get ID of Object to be Edited
+$data = json_decode(file_get_contents("php://input"));
+
+// Set ID Props of Object to be Edited
+$grade->id = $data->id;
+
+// Set Object's Props Values
+$grade->grade_name = $data->grade;
+$grade->major = $data->major;
+
+// Update Object Data
+if ($grade->update()) {
+  // Set Response Code - 200 'OK'
+  http_response_code(200);
+
+  // Tell the User
+  echo json_encode(array("message" => "Kelas telah diupdate!"));
+} else {
+  // If Unable
+  // Set Response Code - 503 'Service Unavailable'
+  http_response_code(503);
+
+  // Tell the User
+  echo json_encode(array("message" => "Kelas gagal diupdate!"));
+}
--- admin/objects/grade.php
+++ admin/objects/grade.php
@@ -81,4 +81,35 @@ class Grade
     $this->grade_name = $row['nama_kelas'];
     $this->major = $row['kompetensi_keahlian'];
   }
+
+  function update()
+  {
+    // Update Query
+    $query = "UPDATE kelas SET
+      id_kelas=:id_kelas, nama_kelas=:nama_kelas, 
+      kompetensi_keahlian=:kompetensi_keahlian
+      WHERE id_kelas=:id_kelas
+    ";
+    
+    // Prepare Query Statement
+    $stmt = $this->conn->prepare($query);
+
+    // Sanitize
+    $this->grade_name = htmlspecialchars(strip_tags($this->grade_name));
+    $this->major = htmlspecialchars(strip_tags($this->major));
+    
+    $this->id = htmlspecialchars(strip_tags($this->id));
+
+    // Bind New Values
+    $stmt->bindParam(":nama_kelas", $this->grade_name);
+    $stmt->bindParam(":kompetensi_keahlian", $this->major);
+
+    $stmt->bindParam(":id_kelas", $this->id);
+
+    // Execute Query
+    if ($stmt->execute()) {
+      return true;
+    }
+    return false;
+  }
 }
