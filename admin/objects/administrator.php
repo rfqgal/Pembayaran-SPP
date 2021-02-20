8a87b3e97a38672d9a3f853f68385a4500d94852
Rifky Galuh <rfqgal@users.noreply.github.com>
2021-02-20 14:56:20 +0700

    created administrator class

---
+++ admin/objects/administrator.php
@@ -0,0 +1,17 @@
+<?php
+class Administrator
+{
+  // Connection and Table Name
+  private $conn;
+
+  // Object Props
+  public $id;
+  public $year;
+  public $fee;
+
+  // Constructor with $db as DB Connection
+  public function __construct($db)
+  {
+    $this->conn = $db;
+  }
+}
